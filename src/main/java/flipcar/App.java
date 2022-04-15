85861207f45b36e25569dfd802048969ec895d52
bob <snm2369@gmail.com>
2022-04-15 18:27:54 +0530

    dev done flipcar ac

---
+++ src/main/java/flipcar/App.java
@@ -0,0 +1,13 @@
+package flipcar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
